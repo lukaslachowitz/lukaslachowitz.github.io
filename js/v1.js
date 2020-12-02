46ec7f0883e191eda31ea475890a94437727fba6
lukaslachowitz <73109755+lukaslachowitz@users.noreply.github.com>
2020-12-02 18:23:52 +0100

    Memory

--- js/v1.js
+++ js/v1.js
@@ -1,14 +1,62 @@
 const numberCards = 16
-
+let openedCards = []
+let cardTypes=[]
+for (let index = 0; index < numberCards/2; index++) {
+    cardTypes.push(index+1)
+    cardTypes.push(index+1)
+}
+cardTypes = shuffle(cardTypes)
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
-        c.innerHTML = index % 2 + 1
+        c.innerHTML = cardTypes[index]
+        c.type = cardTypes[index]
         c.className="card"
         c.addEventListener('click',flipp)
         p.appendChild(c)
 }
 
 function flipp(event){
-    this.classList.toggle('flipped')
+        openCard(this)
+}
+function openCard(c){
+
+   if(openedCards.length<2){
+        c.classList.toggle('flipped')
+        openedCards.push(c)
+        if(openedCards.length == 2){
+            if(openedCards[0].type == openedCards[1].type){
+                window.setTimeout(
+                    ()=>{
+                        openedCards.pop().classList.toggle('found')
+                        openedCards.pop().classList.toggle('found')
+                    },
+                    1000
+                )
+            
+            }
+            else{
+                window.setTimeout(
+                    ()=>{
+                        openedCards.pop().classList.toggle('flipped')
+                        openedCards.pop().classList.toggle('flipped')
+                    },
+                    1000
+                )
+            }
+        }
+
+   }
+}
+
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+    while (ctr > 0) {
+        index = Math.floor(Math.random() * ctr);
+        ctr--;
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
 }
